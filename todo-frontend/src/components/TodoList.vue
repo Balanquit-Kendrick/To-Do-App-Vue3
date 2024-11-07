090dcb4109b24d77a32178f5386fc3ab31f7b563
unknown <zxckendrick123@gmail.com>
2024-11-07 11:52:31 +0800

    added memo display

--- todo-frontend/src/components/TodoList.vue
+++ todo-frontend/src/components/TodoList.vue
@@ -2,7 +2,7 @@
     <div class="flex min-h-screen min-w-full justify-center items-center">
         <div class="min-w-full justify-center">
             <div class="max-w-md h-[600px] mx-auto p-6 bg-[#3C424A] shadow-md rounded-lg overflow-hidden">
-                <h1 class="text-2xl font-bold mb-4 text-center text-gray-400">To-Do List</h1>
+                <h1 class="text-2xl font-bold mb-4 text-center text-gray-300 raleway-large">To-Do List</h1>
                 <form @submit.prevent="addTodo" class="flex mb-4 relative">
                     <SVGAlign class="absolute top-3 left-3" />
                     <input
@@ -18,55 +18,62 @@
                         </p>
                     </div>
                     <div v-else>
-                        <li v-for="todo in todos" :key="todo.id" class="flex items-center mb-2">
-                            <div class="checkbox-wrapper-4 flex-grow">
-                                <input
-                                    class="inp-cbx"
-                                    :id="'todo-' + todo.id"
-                                    v-model="todo.completed"
-                                    @change="toggleCompleted(todo)"
-                                    type="checkbox"
-                                />
-                                <label class="cbx" :for="'todo-' + todo.id">
-                                    <span class="min-w-[18px]">
-                                        <svg width="12px" height="10px">
-                                            <use xlink:href="#check-4"></use>
-                                        </svg>
-                                    </span>
-                                    <span class="text-slate-300 raleway-small">{{ todo.title }}</span>
-                                </label>
-                                <svg class="inline-svg">
-                                    <symbol id="check-4" viewBox="0 0 12 10">
-                                        <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-                                    </symbol>
-                                </svg>
-                            </div>
-                            <div class="relative mr-4">
-                                <button @click="toggleDropdown(todo.id)" class="text-white focus:outline-none">
-                                    <SVGThreeDots/>
-                                </button>
-                                <div v-if="dropdownOpen === todo.id" class="absolute right-0 w-40 mt-2 bg-[#343A40] rounded-md shadow-lg z-10 raleway-medium">
-                                    <ul class="text-gray-300">
-                                        <li @click="openEditModal(todo)" class="flex items-center px-4 py-2 hover:bg-[#3C424A] hover:rounded-t-md cursor-pointer ">
-                                            <SVGEdit />
-                                            Edit task
-                                        </li>
-                                        <li @click="pinTodo(todo)" class="flex items-center px-4 py-2 hover:bg-[#3C424A] cursor-pointer ">
-                                            <SVGPin />
-                                            Pin on the top
-                                        </li>
-                                        <li @click="addMemo(todo)" class="flex items-center px-4 py-2 hover:bg-[#3C424A] cursor-pointer">
-                                            <SVGMemo />
-                                            Add a memo
-                                        </li>
-                                        <li @click="deleteTodo(todo.id)" class="flex items-center px-4 py-2 hover:bg-[#3C424A] hover:rounded-b-md text-red-500 cursor-pointer">
-                                            <SVGTrash />
-                                            Delete
-                                        </li>
-                                    </ul>
+                        <div v-for="todo in todos" :key="todo.id">
+                            <li class="flex items-center mb-[-10px]">
+                                <div class="checkbox-wrapper-4 flex-grow">
+                                    <input
+                                        class="inp-cbx"
+                                        :id="'todo-' + todo.id"
+                                        v-model="todo.completed"
+                                        @change="toggleCompleted(todo)"
+                                        type="checkbox"
+                                    />
+                                    <label class="cbx" :for="'todo-' + todo.id">
+                                        <span class="min-w-[18px]">
+                                            <svg width="12px" height="10px">
+                                                <use xlink:href="#check-4"></use>
+                                            </svg>
+                                        </span>
+                                        <span class="text-slate-300 raleway-small">{{ todo.title }}</span>
+                                    </label>
+                                    
+                                    <svg class="inline-svg">
+                                        <symbol id="check-4" viewBox="0 0 12 10">
+                                            <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                                        </symbol>
+                                    </svg>
                                 </div>
-                            </div>
-                        </li>
+                                
+                                <div class="relative mr-4">
+                                    <button @click="toggleDropdown(todo.id)" class="text-white focus:outline-none">
+                                        <SVGThreeDots/>
+                                    </button>
+                                    <div v-if="dropdownOpen === todo.id" class="absolute right-0 w-40 mt-2 bg-[#343A40] rounded-md shadow-lg z-10 raleway-medium">
+                                        <ul class="text-gray-300">
+                                            <li @click="openEditModal(todo)" class="flex items-center px-4 py-2 hover:bg-[#3C424A] hover:rounded-t-md cursor-pointer ">
+                                                <SVGEdit />
+                                                Edit task
+                                            </li>
+                                            <li @click="pinTodo(todo)" class="flex items-center px-4 py-2 hover:bg-[#3C424A] cursor-pointer ">
+                                                <SVGPin />
+                                                Pin on the top
+                                            </li>
+                                            <li @click="addMemo(todo)" class="flex items-center px-4 py-2 hover:bg-[#3C424A] cursor-pointer">
+                                                <SVGMemo />
+                                                Add a memo
+                                            </li>
+                                            <li @click="deleteTodo(todo.id)" class="flex items-center px-4 py-2 hover:bg-[#3C424A] hover:rounded-b-md text-red-500 cursor-pointer">
+                                                <SVGTrash />
+                                                Delete
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                            <label class="memo">
+                                <span class="ml-[35px] text-slate-400 raleway-small-slant">{{ todo.memo }}</span>
+                            </label>
+                        </div>
                     </div>
                 </ul>
             </div>
